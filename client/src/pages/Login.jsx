46725954f5792898fba0dba19024abed81dcb722
Arvind Yadav <arvindyadav2028@users.noreply.github.com>
2026-02-14 01:40:26 +0530

    2nd Update

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+import '../login.css'
+export default function Login(){
+    const [isLogin, setIsLogin] = useState(true);
+    return(
+        <div className="container">
+            <div className="form-container">
+                <div className="form-toggle">
+    
+                    <button 
+                    className={isLogin ? "active" : ""}
+                    onClick={()=>setIsLogin(true)}
+                    >
+                     Login
+                    </button>
+                    <button 
+                    className={!isLogin ? "active" : ""}
+                    onClick={()=>setIsLogin(false)}
+                    >
+                     Signup
+                    </button>
+                </div>
+                {isLogin ?
+                        <>
+                            <div className="form">
+                                <h2>Login</h2>
+                                <input type="email" placeholder="Enter email"/>
+                                <input type="password" placeholder="Enter Password"/>
+                                <a href="#">Forgot Password?</a>
+                                <button>Login</button>
+                                <p>Not a Member? <a href="#" onClick={()=>setIsLogin(false)}>Signup now</a></p>
+                            </div>
+                        </>:
+                        <>
+                        <div className="form">
+                            <h2>Sign Up</h2>
+                            <input type="email" placeholder="Enter email"/>
+                            <input type="password" placeholder="Enter Password"/>
+                            <input type="password" placeholder="Confirm Password"/>
+                            <button>Signup</button>
+                        </div>
+                    </>   
+                 }
+            </div>
+        </div>
+    )
+}
