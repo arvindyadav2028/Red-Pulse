60bf07a3215179f507f5eac54b4db4b2374499f5
Arvind Yadav <arvindyadav2028@users.noreply.github.com>
2026-02-23 00:53:13 +0530

    4thUpdate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import HomePage from './pages/Home'
-import LoginPage from './pages/LoginPage'
-
 export default function App(){
   return (
-    <LoginPage/>
+    <HomePage/>
   )
 }
